5254a0b47a245d29768f3876284c94e13cefc5f1
Hai Phan <hai.phan@gmail.com>
2024-10-07 14:48:49 -0700

    talk & screen sharing works

--- website/static/js/rtc.js
+++ website/static/js/rtc.js
@@ -1,5 +1,33 @@
 
 function makeRtc(token, isViewer) {
+    const myAudio$ = rxjs.defer(() => {
+        rtc.audioPublished = true
+        const promise = navigator.mediaDevices.getUserMedia({audio: true})
+        return rxjs.from(promise).pipe(
+            rxjs.concatWith(rxjs.NEVER),
+            rxjs.finalize(() => {
+                promise.then(stream => stream.getTracks().forEach(t => t.stop()))
+                rtc.audioPublished = false
+            })
+        )
+    }).pipe(
+        rxjs.shareReplay({bufferSize: 1, refCount: true})
+    )
+
+    const myScreen$ = rxjs.defer(() => {
+        rtc.screenPublished = true
+        const promise = navigator.mediaDevices.getDisplayMedia()
+        return rxjs.from(promise).pipe(
+            rxjs.concatWith(rxjs.NEVER),
+            rxjs.finalize(() => {
+                promise.then(stream => stream.getTracks().forEach(t => t.stop()))
+                rtc.screenPublished = false
+            })
+        )
+    }).pipe(
+        rxjs.shareReplay({bufferSize: 1, refCount: true})
+    )
+
     const rpcHandlers = {
         onChatMessage(text) {
             rtc.chatLog.push({text})
@@ -10,14 +38,12 @@ function makeRtc(token, isViewer) {
         connected: false,
         error: null,
         buddy: null,
-        audioPublished: null,
-        videoPublished: null,
+        audioPublished: false,
+        screenPublished: false,
         chatLog: [],
         setRpcHandler(method, handler) {
             rpcHandlers[method] = handler
         },
-        publishAudio,
-        publishScreen,
     }
 
     if (isViewer) makeViewer()
@@ -34,7 +60,7 @@ function makeRtc(token, isViewer) {
             await new Promise((f,r) => peer.once("open", f).once("error", r))
             rtc.connected = true
             const conn = peer.connect(token, {reliable: true})
-            rtc.buddy = makeBuddy(conn)
+            rtc.buddy = makeBuddy(peer, conn)
         }
         catch (err) {
             console.error(err)
@@ -50,7 +76,7 @@ function makeRtc(token, isViewer) {
             rtc.connected = true
             rxjs.fromEvent(peer, "connection").pipe(
                 rxjs.switchMap(conn => {
-                    rtc.buddy = makeBuddy(conn)
+                    rtc.buddy = makeBuddy(peer, conn)
                     return rxjs.NEVER.pipe(
                         rxjs.finalize(() => conn.close())
                     )
@@ -63,7 +89,8 @@ function makeRtc(token, isViewer) {
         }
     }
 
-    function makeBuddy(conn) {
+    function makeBuddy(peer, conn) {
+        const cleanups = []
         const buddy = {
             connected: false,
             sendRpcMessage(method, args) {
@@ -74,6 +101,38 @@ function makeRtc(token, isViewer) {
                 rtc.chatLog.push({text, isOwn: true})
                 conn.send({method: "onChatMessage", args: [text]})
             },
+            async publishAudio() {
+                let myAudioSub
+                try {
+                    const myAudioStream = await new Promise((f,r) => myAudioSub = myAudio$.subscribe({next: f, error: r}))
+                    const call = peer.call(conn.peer, myAudioStream)
+                    cleanups.push(() => call.close())
+                    await new Promise((f,r) => call.once("close", f).once("error", r))
+                }
+                catch (err) {
+                    console.error(err)
+                    rtc.error = err
+                }
+                finally {
+                    myAudioSub.unsubscribe()
+                }
+            },
+            async publishScreen() {
+                let myScreenSub
+                try {
+                    const myScreenStream = await new Promise((f,r) => myScreenSub = myScreen$.subscribe({next: f, error: r}))
+                    const call = peer.call(conn.peer, myScreenStream)
+                    cleanups.push(() => call.close())
+                    await new Promise((f,r) => call.once("close", f).once("error", r))
+                }
+                catch (err) {
+                    console.error(err)
+                    rtc.error = err
+                }
+                finally {
+                    myScreenSub.unsubscribe()
+                }
+            }
         }
         conn.once("open", () => buddy.connected = true)
         conn.on("error", err => {
@@ -84,51 +143,34 @@ function makeRtc(token, isViewer) {
             if (rpcHandlers[method]) rpcHandlers[method].apply(rpcHandlers, args)
             else console.error(`Missing RPC handler for method '${method}'`)
         })
-        conn.once("close", () => buddy.connected = false)
+        peer.on("call", call => {
+            call.answer()
+            cleanups.push(() => call.close())
+            let isVideo = null
+            call.on("stream", stream => {
+                isVideo = stream.getVideoTracks().length > 0
+                if (isVideo) buddy.videoStream = stream
+                else buddy.audioStream = stream
+            })
+            call.on("error", err => {
+                console.error(err)
+                rtc.error = err
+            })
+            call.once("close", () => {
+                if (isVideo != null) {
+                    if (isVideo) buddy.videoStream = null
+                    else buddy.audioStream = null
+                }
+            })
+        })
+        conn.once("close", () => {
+            buddy.connected = false
+            cleanups.forEach(f => f())
+        })
+        conn.peerConnection.addEventListener("connectionstatechange", () => {
+            //https://bugs.chromium.org/p/chromium/issues/detail?id=982793#c15
+            if (conn.peerConnection.connectionState == "failed") conn.close()
+        })
         return buddy
-    }
-
-    function publishAudio() {
-        if (!audioPublished) {
-            audioPublished = true;
-            return Twilio.Video.createLocalAudioTrack()
-                .then(track => room.localParticipant.publishTrack(track))
-                .catch(err => {
-                    audioPublished = false;
-                    throw err;
-                })
-        }
-        else {
-            return Promise.resolve();
-        }
-    }
-
-    function publishScreen() {
-        if (!screenPublished) {
-            screenPublished = true;
-            return Promise.resolve()
-                .then(() => {
-                    if (!navigator.mediaDevices.getDisplayMedia) throw new Error("Browser doesn't support the Screen Capture API");
-                    return navigator.mediaDevices.getDisplayMedia();
-                })
-                .then(stream => stream.getTracks()[0])
-                .then(mediaStreamTrack => {
-                    const localVideoTrack = new Twilio.Video.LocalVideoTrack(mediaStreamTrack);
-                    return room.localParticipant.publishTrack(localVideoTrack)
-                        .then(() => {
-                            mediaStreamTrack.addEventListener("ended", () => {
-                                room.localParticipant.unpublishTrack(localVideoTrack);
-                                screenPublished = false;
-                            })
-                        })
-                })
-                .catch(err => {
-                    screenPublished = false;
-                    throw err;
-                })
-        }
-        else {
-            return Promise.resolve();
-        }
     }
 }
